e62b104a011b33f2a753079cff40e9275490fa6b
rezch <krudanya1008@gmail.com>
2024-04-22 13:39:50 +0300

    gpt request upd

--- ChatGPT_Request.py
+++ ChatGPT_Request.py
@@ -1,16 +1,59 @@
 from g4f.client import Client
-#from g4f.client import *
 
 
-def Request_Event(appender: str) -> str:
-    initializer = "Сформируй текст в фоме: 1)дата начала: ,2)короткий текст-напоминание связанный с событием, 3)время напоминания в дате. Если ты не знаешь каких-то данных, то ставь -"
+REQUEST_MESSAGE = 'Представь что ты помошник в напоминаниях о событиях. Вот текст события: {}. Найди в нем дату, в формате "дата: %D.%M.%Y", и время, в формате "время: %H:%M", в которое ты должен напомнить об этом событии, а также информацию об этом событии, в формате "событие: "'
 
 
+def RequestEvent(appender: str) -> str:
+    ''' returns response of parsed text from user by gpt4 '''
+
+    initializer = REQUEST_MESSAGE.format(appender)
+
+    # sending request to gpt4
     client = Client()
     response = client.chat.completions.create(
         model="gpt-3.5-turbo",
-        messages=[{"role": "user", "content": initializer + appender}]
+        messages=[{
+            "role": 
+            "user", "content": initializer
+            }]
     )
 
     return response.choices[0].message.content
+
+
+def ParseRequest(response: str) -> dict:
+    ''' returns dict with parsed gpt response
+        if response if invalid, return -1
+    '''
+
+    # result dict
+    data = {
+        'date': '',
+        'time': '',
+        'info': ''
+    }
+
+    # dict with key word for parsing
+    key_words = {
+        'дата': 'date',
+        'время': 'time',
+        'событие': 'info'
+    }
+
+
+    # response preproccessing
+    response = response.split('\n')
+    response = [line.lower().split(':') for line in response]
+
+    if len(response) != 3:
+        return -1
+
+    # data parse    
+    for line in response:
+        if line[0] not in key_words:
+            return -1
+        data[key_words[line[0]]] = line[1].strip()
+
+    return data 
 
---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+BASEDIR=$(dirname "$0")
+
+echo "Starting installing env in '$BASEDIR'"
+
+python3 -m venv $BASEDIR/tmbotenv
+source $BASEDIR/tmbotenv/bin/activate
+
+pip install telebot
+pip install g4f
