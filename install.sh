0a752d1acb5553da249202af45d2b808868c2bb6
rezch <dany1008@yandex.ru>
2024-05-09 00:06:07 +0300

    install script update

--- install.sh
+++ install.sh
@@ -5,13 +5,13 @@ BASEDIR=$(dirname "$0")
 # install python3 if it needed
 if ! type -P python3; then
     echo "installing python3"
-    sudo apt install python3
+    sudo apt install -y python3
 fi
 
 # install python3-venv if it needed
 if ! type -P python3-venv; then
     echo "installing python3-venv"
-    sudo apt install python3-venv
+    sudo apt install -y python3-venv
 fi
 
 echo "starting installing env in '$BASEDIR'"
@@ -19,18 +19,18 @@ python3 -m venv $BASEDIR/tmbotenv
 source $BASEDIR/tmbotenv/bin/activate
 
 echo "updating pip3"
-sudo -H pip3 install --upgrade pip
+sudo -H pip3 install --yes --upgrade pip
 
 echo "pip3 install libs in env"
-pip3 install telebot
-pip3 install g4f
-pip3 install SpeechRecognition
+pip3 install --yes telebot
+pip3 install --yes g4f
+pip3 install --yes SpeechRecognition
 
 echo "apt update in system"
-sudo apt-get update
+sudo apt-get -y update
 
 echo "ffmpeg install in system"
-sudo apt-get remove --purge ffmpeg
-sudo apt-add-repository ppa:mc3man/trusty-media
-sudo apt-add-repository ppa:jonathonf/ffmpeg-3
-sudo apt-get install ffmpeg
+sudo apt-get -y remove --purge ffmpeg
+sudo apt-add-repository -y ppa:mc3man/trusty-media
+sudo apt-add-repository -y ppa:jonathonf/ffmpeg-3
+sudo apt-get -y install ffmpeg
