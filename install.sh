ce8ff57f0fb339fcc8fdfb9f341569aab580ec10
rezch <dany1008@yandex.ru>
2024-05-09 00:46:10 +0300

    ref

--- install.sh
+++ install.sh
@@ -18,21 +18,23 @@ echo "starting installing env in '$BASEDIR'"
 python3 -m venv $BASEDIR/tmbotenv
 source $BASEDIR/tmbotenv/bin/activate
 
-echo "updating pip3"
-sudo -H pip3 install --upgrade pip
-
 echo "pip3 install libs in env"
-pip3 install telebot
-pip3 install g4f
-pip3 install SpeechRecognition
-
-echo "apt update in system"
-sudo apt-get -y update
+if ! pip3 list | grep telebot; then
+  pip3 install telebot
+fi
+if ! pip3 list | grep telebot; then
+  pip3 install g4f
+fi
+if ! pip3 list | grep telebot; then
+  pip3 install SpeechRecognition
+fi
 
 # install ffmpeg if it needed
+if type -P ffmpeg; then
+  sudo apt-get -y remove --purge ffmpeg
+fi
 if ! type -P ffmpeg; then
   echo "ffmpeg install in system"
-  sudo apt-get -y remove --purge ffmpeg
   sudo apt-add-repository -y ppa:mc3man/trusty-media
   sudo apt-add-repository -y ppa:jonathonf/ffmpeg-3
   sudo apt-get -y install ffmpeg
