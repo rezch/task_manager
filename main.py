99e0d26b4f63d20074fcfb16910448bee53e421e
rezch <krudanya1008@gmail.com>
2024-05-10 17:09:02 +0300

    ref.

--- main.py
+++ main.py
@@ -32,6 +32,7 @@ class Bot:
         if message.from_user.last_name is not None:
             if len(result) > 6:
                 result += ' '
+                
             result += message.from_user.last_name
         return result
 
